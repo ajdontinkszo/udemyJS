d14700ddc674316350deff515b40eff857a074bb
Kiss Ferenc <kiss.ferenc.1984@gmail.com>
2016-10-06 22:29:45 +0300

    default values

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var a = 0;
-
-if (a || a === 0) {
-    console.log('Something is there');
+function greet(name) {
+    name = name || '<Your name here>'
+    console.log('Hello ' + name);
 }
+
+greet();
