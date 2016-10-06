26b4ef651aa0323998a224242dc267050eab8c6f
Kiss Ferenc <kiss.ferenc.1984@gmail.com>
2016-10-06 22:23:19 +0300

    existence and booleans

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
-var a = 3 + 4 * 5;
-console.log(a);
-
-var a = 2, b = 3, c = 4;
-a = b = c; //precedence: right-to-left
-
-console.log(a);
-console.log(b);
-console.log(c);
-
-var a = 1 + '2';
-console.log(a);
-
-console.log(3 < 2 < 1); 
-
 var a = 0;
-var b = false;
 
-if (a !== b) {
-    console.log('They are equal!');
-} else {
-    console.log('Nope,  not equal.');
+if (a || a === 0) {
+    console.log('Something is there');
 }
