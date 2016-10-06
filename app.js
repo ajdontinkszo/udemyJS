23e025c3d7df418c8491fa656cd82a8873c70c07
Kiss Ferenc <kiss.ferenc.1984@gmail.com>
2016-10-06 22:16:18 +0300

    operators-precedences-coersion

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-function waitThreeSeconds() {
-    var ms=3000 + new Date().getTime();
-    while (new Date() < ms) {}
-    console.log('finished function');
+var a = 3 + 4 * 5;
+console.log(a);
+
+var a = 2, b = 3, c = 4;
+a = b = c; //precedence: right-to-left
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+var a = 1 + '2';
+console.log(a);
+
+console.log(3 < 2 < 1); 
+
+var a = 0;
+var b = false;
+
+if (a !== b) {
+    console.log('They are equal!');
+} else {
+    console.log('Nope,  not equal.');
 }
-
-function clickHandler() {
-    console.log('click event!');
-}
-
-//listen for the click event
-document.addEventListener('click', clickHandler);
-
-waitThreeSeconds();
-console.log('finished execution');
